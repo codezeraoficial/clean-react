f67d4243e6f47427cb793c2fe96ea7583ebbbe38
Leonardo Viana <leonardo.viana@rarolabs.com.br>
2021-03-10 22:55:51 -0300

    refactor: group imports in index file

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,10 +1,10 @@
+import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { RemoteAuthentication } from './remote-authentication'
 import { UnexpectedError, InvalidCredentialsError } from '@/domain/errors'
 import { mockAccountModel, mockAuthentication } from '@/domain/test'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '@/domain/models/account-model'
+import { AccountModel } from '@/domain/models'
 import faker from 'faker'
 
 type SutTypes = {
