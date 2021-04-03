cd4dfc80df05f6a0a36559b45dc4c3d85064bd38
Leonardo Viana <leonardo.viana@rarolabs.com.br>
2021-04-03 02:03:59 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -4,9 +4,11 @@ import '@testing-library/jest-dom/extend-expect'
 import Login from '.'
 
 describe('Login Component', () => {
-  test('should ', () => {
+  test('Should start with initial state ', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
