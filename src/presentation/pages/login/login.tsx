66acb6bdb0312a155fec6d0e689d0b01e8410238
Leonardo Viana <leonardo.viana@rarolabs.com.br>
2021-09-08 23:22:19 -0300

    feat: ensure Login show password error if Validation fails

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -15,7 +15,7 @@ const Login: React.FC<Props> = ({ validation }) => {
     email: '',
     password: '',
     emailError: '',
-    passwordError: 'Campo obrigatório',
+    passwordError: '',
     mainError: ''
   })
 
@@ -23,17 +23,11 @@ const Login: React.FC<Props> = ({ validation }) => {
     if (validation) {
       setState({
         ...state,
-        emailError: validation.validate('email', state.email)
+        emailError: validation.validate('email', state.email),
+        passwordError: validation.validate('password', state.password)
       })
-      validation.validate('email', state.email)
     }
-  }, [state.email])
-
-  useEffect(() => {
-    if (validation) {
-      validation.validate('password', state.password)
-    }
-  }, [state.password])
+  }, [state.email, state.password])
 
   return (
     <div>
