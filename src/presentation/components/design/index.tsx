967c4f99387b90f84d21780b3fcc6bcc66f1c8dd
Leonardo Viana <leonardo.viana@rarolabs.com.br>
2021-09-13 20:57:07 -0300

    refactor: change import of input

--- src/presentation/components/design/index.tsx
+++ src/presentation/components/design/index.tsx
@@ -1,4 +1,4 @@
 export { default as Button } from './button'
 export { default as Header } from './header'
 export { default as Footer } from './footer'
-export { default as Input } from './input'
+export * from './input'
