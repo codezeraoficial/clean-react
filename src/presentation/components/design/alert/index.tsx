47aaee8b33da11038339c9ffe4a19ec4ce8189a2
Leonardo Viana <leonardo.viana@rarolabs.com.br>
2021-09-14 21:30:54 -0300

    feat: ensure Login Component show error on alert if Authentication fails

--- src/presentation/components/design/alert/index.tsx
+++ src/presentation/components/design/alert/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Snackbar from '@material-ui/core/Snackbar'
+import { Snackbar, SnackbarProps } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
 
-type Props = {
+type Props = SnackbarProps & {
   message: string
   handleClose: () => void
   severity: 'success' | 'info' | 'warning' | 'error'
 }
 
-const Snack: React.FC<Props> = ({ message, handleClose,severity }) => {
+const Snack: React.FC<Props> = ({ message, handleClose, severity, ...props }) => {
   return (
-    <Snackbar open={!!message} autoHideDuration={3000} onClose={handleClose}>
+    <Snackbar open={!!message} autoHideDuration={3000} onClose={handleClose} {...props}>
       <Alert onClose={handleClose} severity={severity} elevation={6} variant="filled">
         {message}
       </Alert>
--- src/presentation/components/login/form-status/index.tsx
+++ src/presentation/components/login/form-status/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import Context from '@/presentation/contexts/form/form-context'
 import { CircularProgress } from '@material-ui/core'
-import Snack from '../../design/alert/snack-bar'
+import Snack from '../../design/alert'
 
 const FormStatus: React.FC = () => {
   const { state: { isLoading, mainError }, setState } = useContext(Context)
@@ -14,8 +14,7 @@ const FormStatus: React.FC = () => {
   return (
     <div data-testid="error-wrap" className="formStatus">
       {isLoading && <CircularProgress size={20} data-testid="spinner" />}
-      {mainError && <span data-testid="main-error">{mainError}</span>}
-      <Snack message={mainError} handleClose={handleClose} severity="error"/>
+      <Snack data-testid="alert-message" message={mainError} handleClose={handleClose} severity="error"/>
     </div>
   )
 }
