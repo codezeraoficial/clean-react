d270b5ce3a34f507b7bc62d52a20a152e118cd4a
Leonardo Viana <leonardo.viana@rarolabs.com.br>
2022-02-15 20:25:07 -0300

    refactor: move sut creation to a factory method

--- src/infra/cache/cookies-adapter.spec.ts
+++ src/infra/cache/cookies-adapter.spec.ts
@@ -4,12 +4,14 @@ import faker from 'faker'
 import cookies from 'js-cookie'
 import { cleanup } from '@testing-library/react'
 
+const makeSut = (): CookiesAdapter => new CookiesAdapter()
+
 describe('CookiesAdapter', () => {
   afterEach(cleanup)
 
   test('Should call cookies with correct values', async () => {
     const setSpy = jest.spyOn(cookies, 'set')
-    const sut = new CookiesAdapter()
+    const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
     await sut.set(key, value)
